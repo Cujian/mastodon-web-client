6b80d130213231c808ffd5ff6947ee6e03c6a5bd
dferdian <dhendyf@gmail.com>
2018-07-24 22:58:26 +1000

    Basic V1 Layout

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,4 +1,9 @@
 class SitesController < ApplicationController
   def index
+    client = Mastodon::REST::Client.new(
+      base_url: ENV["CUJIAN_HOST"],
+      bearer_token: ENV["CUJIAN_ACCESS_TOKEN"]
+    )
+    @public_timeline = client.public_timeline
   end
 end
